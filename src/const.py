d7f56630c8fef46031071e503d285ca133ab58ce
Yael-Hari <yaelhhari@gmail.com>
2024-09-14 10:50:55 -0400

    first commit of data+nbs

---
+++ src/const.py
@@ -0,0 +1,11 @@
+percentage_cols = ['immigrants_post_1990']
+numeric_cols = [
+    'distance_from_tel_aviv', 
+    'council_members_number', 
+    'socioeconomic_level',
+    'compactness_cluster', 
+    'peripheral_cluster'
+]
+only_one_year_needed_col = ['year_of_municipal_status']
+
+DATASET_MATCHING_PATH = 'matching_features_df.csv'
