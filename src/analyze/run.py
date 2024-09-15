bd2bfe964cc52b5beb99eb3e61ca88acfabcef52
gkeren (nlp15) <gkeren@campus.technion.ac.il>
2024-09-15 15:25:26 +0300

    env file and more

---
+++ src/analyze/run.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from src.const import DATASET_MATCHING_PATH
+
+df_matching = pd.read_csv(DATASET_MATCHING_PATH, index_col=False)
+df_treatment = df_matching[df_matching['is_treatment'] == 1]
+df_control = df_matching[df_matching['is_treatment'] == 0]
--- src/matching/matching_funcs.py
+++ src/matching/matching_funcs.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
